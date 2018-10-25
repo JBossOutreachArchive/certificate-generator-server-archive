5dfeeba866a0fe5655b7d35bec40e93b8050d464
Jatin Katyal <jatin.katyal13@gmail.com>
2018-10-25 20:27:34 +0530

    whitenoise middleware added to handle static files in production

--- certificate_generator/settings.py
+++ certificate_generator/settings.py
@@ -40,6 +40,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'certificate_generator.urls'
