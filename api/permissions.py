063e582f90f67d772dc72126f168442cb9fb1066
Jatin Katyal <jatin.katyal13@gmail.com>
2018-10-27 13:36:32 +0530

    removed trailing whitespace and added @classmethod

--- api/permissions.py
+++ api/permissions.py
@@ -2,15 +2,13 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework import permissions
 
-class CanIssueCertificate(permissions.BasePermission):
-    
-    def has_permission(self, request, view):
+class CanIssueCertificate(permissions.BasePermission):   
+    @classmethod
+    def has_permission(cls, request, view):
         user = request.user
-
         if user:
             try:
                 return user.user_permissions.get(codename = 'can_issue_certificate')
             except ObjectDoesNotExist:
                 pass
-        
         return False
