b115a1937b0b9001afcaaaa9b6ec3c53b8aa6815
Isaac Philip <isaac.philip25@gmail.com>
2018-10-30 14:35:17 +0530

    Refactor Code as per PEP8 : 2
    After my previous commit for similar reason, I could
      see that additional merge have added code which fails for PEP8.
    
    I guess in future we need to place the linter in CI to validate tests.
    
    In addition to the code refactoring, removed an import of `render` unused in views

--- certificate_generator/settings.py
+++ certificate_generator/settings.py
@@ -99,7 +99,8 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
