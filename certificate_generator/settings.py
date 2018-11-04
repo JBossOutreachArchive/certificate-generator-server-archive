00457451c97b6c93e36feff068d3289aaf7559d4
aashutoshrathi <aashutoshrathi@gmail.com>
2018-11-04 13:00:36 +0530

    Fixed DB link for production

--- certificate_generator/settings.py
+++ certificate_generator/settings.py
@@ -91,7 +91,16 @@ if DEBUG:
         }
     }
 else:
-    DATABASES = dj_database_url.config(default=config('DATABASE_URL'))
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
+    }
 
 
 # Password validation
