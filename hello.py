3562f9bc2903641e432cbb24aad76bfda04d97e2
OussEmaDevCode <38397893+OussEmaDevCode@users.noreply.github.com>
2018-11-18 14:08:42 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,27 @@
-print("Hello world!")
+from django.test import TestCase
+from .models import Organization, Student
+from django.contrib.auth.models import User
+
+
+class OrganizationTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user("jbossAdmin", None, "jbossAdminsPassword")
+        Organization.objects.create(user=user, name="JBoss Outreach")
+
+    def test_if_organization_was_created(self):
+        user = User.objects.get(username="jbossAdmin")
+        org = Organization.objects.get(user=user)
+
+        self.assertEqual(org.name, "JBoss Outreach")
+
+class StudentTestcase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user("jbossStudent", None, "jbossAdminsPassword")
+        Student.objects.create(user=user, name="Saba Khukhunashvili")
+
+    def test_if_student_was_created(self):
+        user = User.objects.get(username="jbossStudent")
+        student = Student.objects.get(user=user)
+
+        self.assertEqual(student.name, "Saba Khukhunashvili")
+        self.assertEqual(student.email_validated, False)
