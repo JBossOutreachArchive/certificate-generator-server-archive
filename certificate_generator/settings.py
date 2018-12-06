6271163d96c3bb80d3101d52128f564ed5324480
aashutoshrathi <aashutoshrathi@gmail.com>
2018-12-06 21:08:56 +0530

    Move middleware to the top

--- certificate_generator/settings.py
+++ certificate_generator/settings.py
@@ -50,9 +50,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
